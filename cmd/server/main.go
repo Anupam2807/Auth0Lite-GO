50678004f93b8a396809f1181dd1a765256a74f9
Anupam2807 <140740340+Anupam2807@users.noreply.github.com>
2025-08-04 11:35:22 +0530

    Setup the Postgres Database

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,1 +1,31 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+
+	"github.com/Anupam2807/go-auth-service/internal/db"
+)
+
+func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Println("⚠️ No .env file found or failed to load")
+	}
+
+	fmt.Println("DB URL:", os.Getenv("DB_URL"))
+
+	db.Connect()
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Println("Server starting on port", port)
+	http.ListenAndServe(":"+port, nil)
+}
--- internal/db/db.go
+++ internal/db/db.go
@@ -1,1 +1,27 @@
 package db
+
+import (
+	"database/sql"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+var DB *sql.DB
+
+func Connect() {
+	var err error
+	DB, err = sql.Open("postgres", os.Getenv("DB_URL"))
+
+	if err != nil {
+		log.Fatal("DB connection failed:", err)
+
+	}
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("DB ping failed:", err)
+	}
+	log.Println("Connected to PostgreSQL DB")
+
+}
